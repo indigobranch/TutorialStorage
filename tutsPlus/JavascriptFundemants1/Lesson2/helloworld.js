9eb7d41af57f694be53cdea7ff75651563ea7f17
Nels Larsen <github@indigobranch.com>
2012-12-16 03:38:28 -0700

    Lesson 2 completed

---
+++ tutsPlus/JavascriptFundemants1/Lesson2/helloworld.js
@@ -0,0 +1,14 @@
+var myVariable;  // undefined
+myVariable = "Hello";
+
+var numericLiteral = 10;
+var pi = 3.14;
+
+var varBoolean = true;
+
+// Note: camelCase
+var myVar2 = "Second Value";
+
+myvar2 = "Testing";
+
+alert(myVariable + " " + myVar2);
